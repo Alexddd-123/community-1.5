852301f20bb94686706e80e04d79fc629d20e86d
itcast <guohongli0731@163.com>
2022-07-19 10:37:16 +0800

    牛客论坛7.19

--- community/src/main/java/com/nowcoder/community/service/AlphaService.java
+++ community/src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,11 +1,24 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 @Service
 //@Scope("prototype")
@@ -13,6 +26,16 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    //注入一个Bean，通过这个bean管理事务，利用这个bean执行sql，能够保证事务的四个特性
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public AlphaService() {
 //        System.out.println("实例化AlphaService");
@@ -32,4 +55,61 @@ public class AlphaService {
         return alphaDao.select();
     }
 
+
+    //事务的传播机制
+    //REQUIRED：支持当前事务(外部事务),如果不存在创建新事务
+    //REQUIRED_NEW：创建一个新事务，暂停当前事务(外部事务)
+    //NESTED：如果当前存在事务(外部事务)，则嵌套在该事务中执行（独立的提交和回滚，否则就和Required一样）
+
+    //声明式事务
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1() {
+        //新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+        user.setEmail("459962397@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+        //新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("Hello");
+        post.setContent("新人报到");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        return "OK";
+    }
+
+    //编程式事务
+    public Object save2() {
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus transactionStatus) {
+                //新增用户
+                User user = new User();
+                user.setUsername("sdgsg");
+                user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+                user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+                user.setEmail("459962397@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+                //新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("kkk");
+                post.setContent("oooo");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+                return "OK";
+            }
+        });
+    }
 }
